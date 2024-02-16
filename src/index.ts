4ff8828a503351d3f05106566cc63ca927b8ce49
Maiko Tan <maiko.tan.coding@gmail.com>
2024-02-16 09:07:08 +0800

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Context, h, Schema } from 'koishi'
+
+export const name = 'tarot'
+export const Config = Schema.object({})
+
+export function apply(ctx: Context) {
+  ctx
+    .command('tarot')
+    .alias('塔罗')
+    .alias('塔罗牌')
+    .action(async () => {
+      let result = await getTarot(ctx)
+      let img = result.match(/(?<=(±img=)).+(?=±)/g)[0]
+      let text1 = result.match(/.+(?=(±i))/g)[0].replace(/(\\r)/, '\n')
+      let text2 = result.match(/(?<=(g±)).+/g)[0].replace(/(\\r)/, ' ')
+      return h('message', text1, h('image', { url: img }), text2)
+    })
+}
+
+async function getTarot(ctx) {
+  let result = await ctx.http('get', 'http://api.tangdouz.com/tarot.php')
+  return result
+}
